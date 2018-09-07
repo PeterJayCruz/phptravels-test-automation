13e223b5ac9d0b3cbedca2ede7bd8c93168662e1
PeterJayCruz <pcruz1@mail.sfsu.edu>
2018-09-07 15:34:28 -0700

    deleted unimplemented functions

--- src/test/setup/SetupTest.java
+++ src/test/setup/SetupTest.java
@@ -1,35 +1,22 @@
 package setup;
 
-import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 
 public class SetupTest {
 	
 	private WebDriver driver;
-	
-	@Test
-	public void f() {
-	}
-
-	@BeforeClass
-	public void beforeClass() {
-	}
-
-	@AfterClass
-	public void afterClass() {
-	}
 
 	@BeforeMethod
 	public void initializeFirefoxDriver() {
 		System.setProperty("webdriver.gecko.driver", "/Users/PeterJayCruz/Desktop/workspace/_libs/geckodriver");
 		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 	}
 	
 	@AfterMethod
