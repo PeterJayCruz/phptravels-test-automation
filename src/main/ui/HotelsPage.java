5d7c54fd82b4a932cc819394ef609aaf2b10a8ac
PeterJayCruz <pcruz1@mail.sfsu.edu>
2018-09-07 19:02:40 -0700

    added filter section menu buttons and methods to click each filter menu button

--- src/main/ui/HotelsPage.java
+++ src/main/ui/HotelsPage.java
@@ -15,6 +15,28 @@ public class HotelsPage extends BasePage {
 	@FindBy(xpath = "//a[@href='#collapseMap']")
 	private WebElement viewMapButton;
 
+	@FindBy(css = "div[class*='search-button']")
+	private WebElement searchButton;
+	
+	/*
+	 * Filter section elements
+	 */
+	@FindBy(xpath = "//button[contains(text(),'Star Grade')]")
+	private WebElement starGradeMenuButton;
+	
+	@FindBy(xpath = "//button[contains(text(),'Price Range')]")
+	private WebElement priceRangeMenuButton;
+	
+	@FindBy(xpath = "//button[contains(text(),'Property Types')]")
+	private WebElement propertyTypesMenuButton;
+	
+	@FindBy(xpath = "//button[contains(text(),'Amenities')]")
+	private WebElement amenitiesMenuButton;
+
+	@FindBy(id = "searchform")
+	private WebElement filterSearchButton;
+	
+	
 	public HotelsPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
@@ -24,4 +46,35 @@ public class HotelsPage extends BasePage {
 		driver.get(HOTELS_PAGE_URL);
 	}
 
+	public void clickViewMapButton() {
+		clickElement(driver, viewMapButton);
+	}
+
+	public void clickSearchButton() {
+		clickElement(driver, searchButton);
+	}
+	
+	
+	/*
+	 * Filter section methods
+	 */
+	public void clickStarGradeMenuButton() {
+		clickElement(driver, starGradeMenuButton);		
+	}
+	
+	public void clickPriceRangeMenuButton() {
+		clickElement(driver, priceRangeMenuButton);
+	}
+	
+	public void clickPropertyTypesMenuButton() {
+		clickElement(driver, propertyTypesMenuButton);
+	}
+
+	public void clickAmenitiesMenuButton() {
+		clickElement(driver, amenitiesMenuButton);
+	}
+	
+	public void clickFilterSearchButton() {
+		clickElement(driver, filterSearchButton);
+	}
 }
