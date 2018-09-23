c5fab20f7cd00a6c5141db418d00ea06bca2f1df
PeterJayCruz <pcruz1@mail.sfsu.edu>
2018-09-22 17:22:40 -0700

    added int values for StarGrades

--- src/main/ui/hotelspage/StarGrades.java
+++ src/main/ui/hotelspage/StarGrades.java
@@ -2,19 +2,25 @@ package ui.hotelspage;
 
 public enum StarGrades {
 
-	ONE_STAR(0),
-	TWO_STAR(1),
-	THREE_STAR(2),
-	FOUR_STAR(3),
-	FIVE_STAR(4);
-	
+	ONE_STAR(0, 1),
+	TWO_STAR(1, 2),
+	THREE_STAR(2, 3),
+	FOUR_STAR(3, 4),
+	FIVE_STAR(4, 5);
+
 	private final int index;
-	
-	private StarGrades(int index) {
+	private final int value;
+
+	private StarGrades(int index, int value) {
 		this.index = index;
+		this.value = value;
 	}
-	
+
 	public int getIndex() {
 		return index;
+	}
+
+	public int getValue() {
+		return value;
 	}
 }
