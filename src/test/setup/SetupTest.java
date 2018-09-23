1aa249db511e2879d6a8a6411e99fa0032fa3ac6
PeterJayCruz <pcruz1@mail.sfsu.edu>
2018-09-22 17:34:40 -0700

    removed implicit wait from SetupTest.java

--- src/test/setup/SetupTest.java
+++ src/test/setup/SetupTest.java
@@ -9,23 +9,23 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 
 public class SetupTest {
-	
+
 	private WebDriver driver;
 
 	@BeforeMethod
 	public void initializeFirefoxDriver() {
 		System.setProperty("webdriver.gecko.driver", "/Users/PeterJayCruz/Desktop/workspace/_libs/geckodriver");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
-	
+
 	@AfterMethod
 	public void quitBrowser() {
-		if(driver != null) {			
+		if(driver != null) {
 			driver.quit();
 		}
 	}
-	
+
 	public WebDriver getDriver() {
 		return driver;
 	}
